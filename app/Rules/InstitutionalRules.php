64f6bf43b795e6d007c0775ed4bcc299ac6add3e
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2025-08-02 17:35:29 -0300

    Regras institucionais

--- app/Rules/InstitutionalRules.php
+++ app/Rules/InstitutionalRules.php
@@ -1,15 +1,16 @@
 <?php
 
 namespace App\Rules;
+use Illuminate\Validation\Rule;
 
 class InstitutionalRules
 {
-    public static function rules(): array
+    public static function rules(?int $id = null): array
     {
         return [
             'title' => 'required|string',
             'value' => 'required|string',
-            'code' => 'required|string|unique:institutionals,code',
+            'code' => ['required', 'string', Rule::unique('institutionals', 'code')->ignore($id)],
             'active' => 'required|boolean',
         ];
     }
