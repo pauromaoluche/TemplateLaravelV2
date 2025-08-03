f00c32a6aa3eddc1349007543ebe35cbeacbde6c
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2025-08-03 18:02:34 -0300

    Ajustes

--- resources/views/dashboard/institutional/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('dashboard.layouts.app')
-@section('title', 'Institucional')
-@section('content')
-    <x-dashboard.ui.breadcrumb />
-    <livewire:dashboard.list-item :route="$route" :columns="[['column' => 'title', 'width' => '30%'], ['column' => 'value', 'width' => '30%']]" :model="$model" />
-@endsection
